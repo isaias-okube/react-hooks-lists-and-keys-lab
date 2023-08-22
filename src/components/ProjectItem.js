1ca9d191225bf618cf9147464988c2639bfe7afc
ISAIAS <issuyep@yahoo.com>
2023-08-21 20:42:20 -0600

    complete lab

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -6,9 +6,13 @@ function ProjectItem({ name, about, technologies }) {
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
-      </div>
+        {technologies.map((technology, index) => (
+            <span key={index} className="technology">
+              {technology}</span>
+        /* render a <span> for each technology in the technologies array */
+          ))}
     </div>
+  </div>
   );
 }
 
